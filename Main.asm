86d6b93f7a357f98c5cb3b6843c30c8d4807d6fc
Maddi Sikorski <madilynsikorski@yahoo.com>
2018-12-06 22:34:01 -0600

    added main file

---
+++ Main.asm
@@ -0,0 +1,8 @@
+; Main.asm
+; Name:
+; UTEid: 
+; Continuously reads from x4600 making sure its not reading duplicate
+; symbols. Processes the symbol based on the program description
+; of mRNA processing.
+               .ORIG x4000
+.END
