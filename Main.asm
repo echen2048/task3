0271351ab281cc9a8a392175f6673affb472320a
echen <eric.c@utexas.edu>
2018-12-06 22:41:53 -0600

    initialized interrupts for lc3 in main

--- Main.asm
+++ Main.asm
@@ -5,4 +5,25 @@
 ; symbols. Processes the symbol based on the program description
 ; of mRNA processing.
                .ORIG x4000
+; initialize the stack pointer
+LD R6,BOS
+; set up the keyboard interrupt vector table entry
+LD R0,ISR
+STI R0,KBIVE
+
+; enable keyboard interrupts
+LD R0,KBIEN
+STI R0,KBSR
+
+; start of actual program
+
+
+KBIEN .FILL x4000
+KBSR .FILL xFE00
+KBIVE .FILL x0180
+ISR .FILL x2600
+data .FILL x4600
+
+BOS .FILL x4000
+
 .END
